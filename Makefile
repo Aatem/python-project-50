76a228e1c0e8ba35cd509fb11c602b6681296e52
artem <harchenkoart@gmail.com>
2023-04-06 19:08:30 +0300

    step one, add gendiff

---
+++ Makefile
@@ -0,0 +1,20 @@
+install:
+	poetry install
+
+gendiff:
+	poetry run gendiff
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python3 -m pip install --user dist/*.whl
+
+lint:
+	poetry run flake8 brain_games
+
+package-reinstall:
+	pip install --user --force-reinstall dist/*.whl
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+
+args = parser.parse_args()
+
+print(args)
+
+def main():
+    
+    return print('Временный текст!')
+
+if __name__ == '__main__':
+    main()
