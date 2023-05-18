39096ff8f5aca0516c128470a57d2b0655e18840
artem <harchenkoart@gmail.com>
2023-05-18 13:13:00 +0300

    redacted test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,6 +3,6 @@ from gendiff.generator_diff import generate_diff
 
 def test_gendiff_json():
     
-    result_json = generate_diff('/home/artem/python-project-50/tests/fixtures/file1.json', '/home/artem/python-project-50/tests/fixtures/file2.json')
-    true_result_json = open('/home/artem/python-project-50/tests/fixtures/true_result').read()
+    result_json = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    true_result_json = open('tests/fixtures/true_result').read()
     assert result_json == true_result_json
